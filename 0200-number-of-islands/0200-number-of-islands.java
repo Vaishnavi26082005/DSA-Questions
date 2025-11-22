1221e5d76d6b4a681c99a3edf57b3d1c06fda314
Vaishnavi Sharma <vaishnavicool1234@gmail.com>
2025-11-22 19:43:14 +0530

    Time: 5 ms (32.07%), Space: 54.7 MB (5.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,25 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int n=grid.length;
-        int m=grid[0].length;
+        int m= grid[0].length;
+        boolean[][] arr= new boolean [n][m];
         int count=0;
-        boolean [][] visi= new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]!='0'&&!visi[i][j]){
-                    dfs(grid,visi,i,j);
-                    count++;
-                }
+                if(grid[i][j]!='0'&&!arr[i][j]){
+                dfs(grid, arr,i, j, n, m);
+                count++;}
             }
         }
         return count;
     }
-    public static void dfs(char[][]grid,boolean[][]visi,int r,int c){
-         int n=grid.length;
-        int m=grid[0].length;
-        if(r>=n||r<0||c>=m||c<0||visi[r][c]||grid[r][c]=='0')return ;
-        int k[]={0,0,-1,1};
-        int l[]={1,-1,0,0};
-        visi[r][c]=true;
-        for(int i=0;i<k.length;i++){
-
-            dfs(grid,visi,r+k[i],c+l[i]);
+    public void dfs(char[][] grid,boolean [][] arr,int i,int j,int n,int m){
+        if(i<0||j<0||i>=n||j>=m||grid[i][j]=='0'||arr[i][j])return ;
+        arr[i][j]=true;
+        int r[]={0,0,-1,1};
+        int c[]={1,-1,0,0};
+        for(int k=0;k<r.length;k++){
+            dfs(grid,arr,i+r[k],j+c[k],n,m);
         }
     }
 }

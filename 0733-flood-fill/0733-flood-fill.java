cd7522c3f0b3744356e663693ee8451dcc94bfbb
Vaishnavi Sharma <vaishnavicool1234@gmail.com>
2025-11-22 18:59:17 +0530

    Time: 1 ms (43.91%), Space: 46.3 MB (33.3%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int er=image.length;
-        int ec=image[0].length;
-        boolean visi[][]=new boolean[er][ec];
-        solve(sr,sc,er,ec,visi,color,image,image[sr][sc]);
+        int ec= image[0].length;
+        boolean arr[][]= new boolean [er][ec];
+        solve(image,sr,sc,er,ec,image[sr][sc],color,arr);
         return image;
     }
-    public void solve(int sr,int sc,int er,int ec,boolean[][]visi,int nc,int image[][],int cc){
-        if(sr<0||sc<0||sr>=er||sc>=ec||image[sr][sc]!=cc||visi[sr][sc])return ;
-        visi[sr][sc]=true;
+    public void solve(int[][]image, int sr,int sc,int er,int ec, int cc,int nc,boolean[][] arr){
+
+        if(sr<0||sc<0||sr>=er||sc>=ec||image[sr][sc]!=cc||arr[sr][sc])return ;
+        arr[sr][sc]=true;
         image[sr][sc]=nc;
-        int r[]={0,0,-1,1};
+        int r[]={0,0,1,-1};
         int c[]={1,-1,0,0};
-        for(int i=0;i<r.length;i++){
-            solve(sr+r[i],sc+c[i],er,ec,visi,nc,image,cc);
+        for(int i=0;i<c.length;i++){
+            solve(image,sr+r[i],sc+c[i],er,ec,cc,nc,arr);
         }
     }
 }
